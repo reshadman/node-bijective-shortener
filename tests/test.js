51663e5ddb9c34efd25001f9cbef793fcb4977cd
Reza Shadman <pcfeeler@gmail.com>
2015-03-17 11:03:00 +0330

    Add tests for bijective shortener module

---
+++ tests/test.js
@@ -0,0 +1,18 @@
+var assert = require("assert");
+var bijective = require("../src/bijective-shortener.js");
+
+describe("BijectiveShortener", function(){
+	describe("#makeFromInteger", function(){
+		it("should make unique strings for unique integers and decode shortened to original integer on base 10", function(){
+			var obj = {},
+				shortened;
+
+			for(var i = 500000; i < 600000; i++){
+				shortened = bijective.makeFromInteger(i);
+				assert.equal(false, obj.hasOwnProperty(shortened));
+				assert.equal(i, bijective.decodeToInteger(shortened));
+				obj.shortened = i;
+			}
+		});
+	});
+});
